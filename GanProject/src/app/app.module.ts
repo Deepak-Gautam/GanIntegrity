5b56c53efa55d808b2cad6aed81c584369ea1503
Anina Mahat <dipakgautam120@gmail.com>
2021-06-02 00:09:43 +0200

    adding necessary components

--- GanProject/src/app/app.module.ts
+++ GanProject/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { BooksComponent } from './books/books.component';
+import { SongsComponent } from './songs/songs.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    BooksComponent,
+    SongsComponent
   ],
   imports: [
     BrowserModule,
