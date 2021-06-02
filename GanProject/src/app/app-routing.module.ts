e2570f84f1a84066c3b8b7a9494503a1c390dc25
Deepak Gautam <dipakgautam120@gmail.com>
2021-06-02 11:32:17 +0200

    setting routes

--- GanProject/src/app/app-routing.module.ts
+++ GanProject/src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BooksComponent } from './books/books.component';
+import { HomeComponent } from './home/home.component';
+import { SongsComponent } from './songs/songs.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'songs', component: SongsComponent },
+  { path: 'books', component: BooksComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ GanProject/src/app/services/in-memory-data.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InMemoryDataService {
+
+  createDb(){
+    const books: Object={
+      
+    }
+  }
+  constructor() { }
+}
